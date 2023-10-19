4d75514d1cca4832caa70f3a970526330a2a100a
Hrivaux <hugorivaux@gmail.com>
2023-10-19 09:46:26 +0200

    d

--- assets/php/header.php
+++ assets/php/header.php
@@ -1,6 +1,3 @@
-
-
-
 <?php
 include('assets/php/sql.php')
 ?>
@@ -22,7 +19,7 @@ include('assets/php/sql.php')
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css?1" rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
   <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
@@ -33,23 +30,23 @@ include('assets/php/sql.php')
 
 </head>
 <header id="header" class="fixed-top header-transparent">
-    <div class="container d-flex align-items-center justify-content-between position-relative">
+  <div class="container d-flex align-items-center justify-content-between position-relative">
 
-      <div class="logo">
-        <h1 class="text-light"><a href="index.php"><span>Sellerie Personeni</span></a></h1>
-      </div>
-
-      <nav id="navbar" class="navbar">
-        <ul>
-          <li><a class="nav-link scrollto active" href="index.php#hero">Accueil</a></li>
-          <li><a class="nav-link scrollto" href="index.php#about">À propos de moi</a></li>
-          <!--<li><a class="nav-link scrollto" href="#services">Nos prjets </a></li>-->
-          <li><a class="nav-link scrollto" href="index.php#services">Mes services</a></li>
-          <li><a class="nav-link scrollto" href="index.php#portfolio">Mes créations</a></li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav>
-
+    <div class="logo">
+      <h1 class="text-light"><a href="index.php"><span>Sellerie Personeni</span></a></h1>
     </div>
-  </header>
+
+    <nav id="navbar" class="navbar">
+      <ul>
+        <li><a class="nav-link scrollto active" href="index.php#hero">Accueil</a></li>
+        <li><a class="nav-link scrollto" href="index.php#about">À propos de moi</a></li>
+        <!--<li><a class="nav-link scrollto" href="#services">Nos prjets </a></li>-->
+        <li><a class="nav-link scrollto" href="index.php#services">Mes services</a></li>
+        <li><a class="nav-link scrollto" href="index.php#portfolio">Mes créations</a></li>
+        <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+      </ul>
+      <i class="bi bi-list mobile-nav-toggle"></i>
+    </nav>
+
+  </div>
+</header>
